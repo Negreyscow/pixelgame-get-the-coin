922662d67d162be34e43b8e155426892038189af
Negreyscow <leeo.apc@gmail.com>
2020-01-02 09:14:46 +0000

    remove/add players and coins

---
+++ playground/1st-release/keyboard-listener.js
@@ -0,0 +1,35 @@
+function createKeyboardListener(){
+
+    //observer pattern
+    const state = {
+        observers: [] //lista dos observers
+    }
+    
+    function subscribe(observerFunction){ //registrar observer dentro de um subject
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command){
+        console.log(`Notifying ${state.observers.length} observers`)
+        for (const observerFunction of state.observers){
+            observerFunction(command)
+        }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    function handleKeyDown(event){
+        const keyPressed = event.key
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        } 
+        notifyAll(command)
+    }
+
+    return {
+        subscribe
+    }
+
+}
